12e782b837348eecdd2130dd6775798f5efc1b8a
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-11-28 09:08:56 +0200

    Plan

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 # How to run:
 # python main.py path_to_input_file
 
+# 1 - Implement basic arithmetic
+# 2 - Implement nested expressions
+# 3 - Implement multi-lined processing
+# (\n to end expression, \ to continue expression in next line)
+# 4 - Implement variables
+# 5 - Implement function calls
+
 # Tokens:
 # Numbers
 # + - * /
 
 import sys
+from sys import argv
 
-input_file = open(sys.argv[0], 'r')
+input_file = open(argv[0], 'r')
 
 # Print input file
 for line in input_file.readlines():
