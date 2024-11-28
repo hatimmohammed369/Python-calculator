2d377e4ab82f0d2eb1cc51d2bec579f5fe7e05db
Hatim Muhammed <hatimmohammed369@gmail.com>
2024-11-28 09:10:30 +0200

    Token class

--- main.py
+++ main.py
@@ -12,7 +12,7 @@
 # Numbers
 # + - * /
 
-import sys
+from enum import Enum
 from sys import argv
 
 input_file = open(argv[0], 'r')
@@ -21,3 +21,20 @@ input_file = open(argv[0], 'r')
 for line in input_file.readlines():
     print(line, end='')
 print()
+
+
+class TokenType(Enum):
+    NUMBER = 1
+    PLUS = 2
+    MINUS = 3
+    STAR = 4
+    SLASH = 5
+
+
+class Token:
+    # Constructor
+    def __init__(self, ttype: TokenType, value: str):
+        self.ttype = ttype
+        self.value = value
+
+
